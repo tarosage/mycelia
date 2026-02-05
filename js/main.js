9741e4810c64bacbd7b6c20cbbb41ef8d517ef48
Taro <coleol@oregonstate.edu>
2026-02-05 13:41:33 -0800

    adding points to map

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 
-//initializing map and setting view (currently london)
- var map = L.map('map').setView([44.0521, 123.0897], 13);
+//initializing map and setting view to OSU
+ var map = L.map('map').setView([44.5646, 123.2620], 13);
 
  //adding tiles
  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+//Calling the Data
+fetch('/Users/tarocole/Desktop/mycelia/data/fungi on campus')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data) .addTo(map);
+    })
+    .catch(error => console.error('Error: '))
