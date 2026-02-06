022dd6a05b181c4f4be276255673b3d8f5ac6346
Taro <coleol@oregonstate.edu>
2026-02-05 17:17:56 -0800

    adding dynamic elements

--- js/main.js
+++ js/main.js
@@ -1,17 +1,1 @@
 
-//initializing map and setting view to OSU
- var map = L.map('map').setView([44.5646, 123.2620], 13);
-
- //adding tiles
- L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-//Calling the Data
-fetch('/Users/tarocole/Desktop/mycelia/data/fungi on campus')
-    .then(response => response.json())
-    .then(data => {
-        L.geoJSON(data) .addTo(map);
-    })
-    .catch(error => console.error('Error: '))
