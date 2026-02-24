8fcc5e7667b37c8b9b6e795bfa4a3ab5833265d1
Taro <coleol@oregonstate.edu>
2026-02-23 22:40:01 -0800

    turning this into lab 4 by using my own mapbox style or whatever

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-
 //initializing map and setting view to OSU
-
 var map = L.map('map').setView([44.5646, -123.2620], 13);
 
 //adding tiles
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://api.mapbox.com/styles/v1/tarosage/cmm05cp7o003t01su2jom52sy/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGFyb3NhZ2UiLCJhIjoiY21semloeDZwMDc3ZzNlcTJ0ZmNvYjZ3eiJ9.gz6DoGzeLKlwzKzmeDveUQ', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
 
 //custom marker icon
 
@@ -49,3 +48,25 @@ fetch('data/campusfungi/mushrooms.json')
   .catch(err =>
     console.error('Error connecting to mycelial network.', err)
   );
+
+
+
+   
+/* 
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidGFyb3NhZ2UiLCJhIjoiY21semloeDZwMDc3ZzNlcTJ0ZmNvYjZ3eiJ9.gz6DoGzeLKlwzKzmeDveUQ';
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/tarosage/cmm05cp7o003t01su2jom52sy', 
+        projection: 'globe', // display the map as a globe
+        zoom: 12, // initial zoom level, 0 is the world view, higher values zoom in
+        center: [-123.2620, 44.5646]
+    });
+
+    map.addControl(new mapboxgl.NavigationControl());
+    map.scrollZoom.disable();
+
+    map.on('style.load', () => {
+        map.setFog({}); // Set the default atmosphere style
+    });
+    */
